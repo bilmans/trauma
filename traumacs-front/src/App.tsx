cfdae47d16365678e9fdaad5e65dabeb63e57b1c
Alexandre Pierre <alexandre.pierre@student-cs.fr>
2022-11-21 15:57:21 +0100

    Set up prettier + Fix on save

--- traumacs-front/src/App.tsx
+++ traumacs-front/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function App() {
